4e7cd0111c6cae0439d46497ab3f4806fae04a05
Anton Romanov <romanton8@gmail.com>
2019-05-08 13:31:08 +0300

    - add PrivateStaticData structure;

---
+++ SearchingTest/Supporting Files/PrivateStaticData.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateStaticData.swift
+//  SearchingTest
+//
+//  Created by Anton Romanov on 08/05/2019.
+//  Copyright © 2019 Anton Romanov. All rights reserved.
+//
+
+import Foundation
+
+struct PrivateStaticData {
+    // TODO: - Add your personal 'LastFM' API key
+    static let lastFmApiKey = ""
+}
